bdf223221bd4c3179681cb6586c72d4d625dea2f
Arturo Hernandez Medero <arturo.hernandez@macropay.mx>
2024-11-12 17:04:23 -0600

    feat(): test placeholder

---
+++ app/assets/config.ts
@@ -0,0 +1,4 @@
+export const BASE_PATH_PLACEHOLDER="https://jsonplaceholder.typicode.com";
+
+export const GET_POSTS = `${BASE_PATH_PLACEHOLDER}/posts`;
+export const GET_USERS = `${BASE_PATH_PLACEHOLDER}/users`;
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,94 @@
+'use client'
+
+import { GET_POSTS, GET_USERS } from '@/app/assets/config';
+import { useEffect, useState } from 'react';
+
+export interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  website: string,
+  phone: string,
+  email: string,
+  company: {
+    bs: string,
+    catchPhrase: string
+    name: string
+  },
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string
+    }
+  },
+}
+
 export default function page() {
+
+  const [users, setUser] = useState<User[]>([])
+  const [posts, setPost] = useState<Post[]>([])
+
+  useEffect(() => {
+    fetch(GET_USERS)
+      .then((response) => response.json())
+      .then((json) => {
+        setUser(json);
+      });
+  }, [])
+
+  const getPost = useEffect(() => {
+    fetch(GET_USERS)
+      .then((response) => response.json())
+      .then((json) => {
+        setUser(json);
+      });
+  }, [])
+
+  function getPostByUserId(userId: number) {
+    fetch(`${GET_POSTS}/${userId}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setPost(json);
+        console.log(posts);
+      }); 
+  }
+
+  function buscar(id: number) {
+    getPostByUserId(id);
+  }
+
   return (
     <div>
-      Customers page works!
+
+      <br /><br />
+
+      <ul>
+        {
+          users?.map((user: User) => {
+            return (
+              <>
+                <section key={user.id}>
+                  <li>{user.id} | {user.name} | {user.username} | {user.email}</li>
+                  <button className="border p-5 bg-background" onClick={() => {
+                    buscar(user.id)
+                  }}> Get post by id </button>
+                </section>
+              </>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
